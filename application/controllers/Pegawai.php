efbb463e0339036903a4538b788744f4a8df3ce1
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-12-28 19:00:24 +0700

    override username as nip

--- application/controllers/Pegawai.php
+++ application/controllers/Pegawai.php
@@ -70,6 +70,7 @@ class Pegawai extends CI_Controller {
             $_POST['pegawai']['foto'] = $file;
 
             $_POST['user']['nama'] = $_POST['pegawai']['nama'];
+	    $_POST['user']['username'] = $_POST['pegawai']['nip'];
             $_POST['user']['password'] = password_hash($_POST['user']['password'],PASSWORD_DEFAULT);
             $_POST['user']['is_active'] = 'Ya';
 
